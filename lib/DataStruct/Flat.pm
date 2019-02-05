57f586a4363c4ba29c4cf90be8abddb9ecb01ad6
Jose Luis Martinez <jlmartinez@capside.com>
2019-02-05 14:10:18 +0000

    Documentation

--- lib/DataStruct/Flat.pm
+++ lib/DataStruct/Flat.pm
@@ -35,3 +35,69 @@ package DataStruct::Flat;
     }
   }
 1;
+
+#################### main pod documentation begin ###################
+
+=head1 NAME
+
+DataStruct::Flat - Convert a data structure into a one level list of keys and values
+
+=head1 SYNOPSIS
+
+  use DataStruct::Flat;
+
+  my $flattener = DataStruct::Flat->new;
+
+  my $flat = $flattener->flatten({
+    a => [ 7, 8, 9, 10 ],
+    b => { c => d },
+  });
+
+  # $flat = {
+  #   'a.0' => 7,
+  #   'a.1' => 8,
+  #   'a.2' => 9,
+  #   'a.3' => 10,
+  #   'b.c' => 'd'
+  # };
+
+=head1 DESCRIPTION
+
+This module converts a nested Perl data structure into a one level hash of keys and values
+apt for human consumption.
+
+=head1 METHODS
+
+=head2 new
+
+Constructor. Initializes the flattener object
+
+=head2 flatten($struct)
+
+Returns a hashref for $struct which contains keys with dotted "paths" to the respective
+values in the datastructure.
+
+=head1 CONTRIBUTE
+
+The source code is located here: https://github.com/pplu/datastruct-flat
+
+=head2 SEE ALSO
+
+L<Hash::Flatten>
+
+=head1 AUTHOR
+    Jose Luis Martinez
+    CPAN ID: JLMARTIN
+    CAPSiDE
+    jlmartinez@capside.com
+    http://www.pplusdomain.net
+
+=head1 COPYRIGHT
+
+Copyright (c) 2019 by CAPSiDE
+
+=head1 LICENSE
+
+Apache 2.0
+
+=cut
